e3c287237393724e1bf224c97321d2e5709b7604
programandoconro <programandoconro@gmail.com>
2023-05-03 00:09:48 +0900

    add translations for timeline

--- src/scripts/setContent.js
+++ src/scripts/setContent.js
@@ -1,15 +1,23 @@
 import * as introductionText from "../../i18n/introdution.json";
 import * as profilesText from "../../i18n/profiles.json";
+import * as timelineText from "../../i18n/timeline.json";
 import * as LANGUAGES from "../../i18n/languages.json";
 
 const intro = document.getElementById("intro");
 const profileTitle = document.getElementById("profile-title");
+
 const githubProfile = document.getElementById("github-profile");
 const stackoverflowProfile = document.getElementById("stackoverflow-profile");
 const hackerrankProfile = document.getElementById("hackerrank-profile");
 const wordpressProfile = document.getElementById("wordpress-profile");
 const linkedinProfile = document.getElementById("linkedin-profile");
 const flickrProfile = document.getElementById("flickr-profile");
+
+const ucTimeline = document.getElementById("uc-timeline");
+const ivicTimeline = document.getElementById("ivic-timeline");
+const freelancerTimeline = document.getElementById("freelancer-timeline");
+const lateralTimeline = document.getElementById("lateral-timeline");
+const rakutenTimeline = document.getElementById("rakuten-timeline");
 
 const joinText = (text) => {
   return text.join(" ");
@@ -18,6 +26,7 @@ const joinText = (text) => {
 const setContent = (language) => {
   intro.innerText = joinText(introductionText[language]);
   profileTitle.innerText = joinText(profilesText.title[language]);
+
   githubProfile.innerText = joinText(profilesText.github[language]);
   stackoverflowProfile.innerText = joinText(
     profilesText.stackoverflow[language]
@@ -26,6 +35,12 @@ const setContent = (language) => {
   wordpressProfile.innerText = joinText(profilesText.wordpress[language]);
   linkedinProfile.innerText = joinText(profilesText.linkedin[language]);
   flickrProfile.innerText = joinText(profilesText.flickr[language]);
+
+  ucTimeline.innerText = joinText(timelineText.uc[language]);
+  ivicTimeline.innerText = joinText(timelineText.ivic[language]);
+  freelancerTimeline.innerText = joinText(timelineText.freelancer[language]);
+  lateralTimeline.innerText = joinText(timelineText.lateral[language]);
+  rakutenTimeline.innerText = joinText(timelineText.rakuten[language]);
 };
 const getLanguage = () => {
   const language = localStorage.getItem("language");
