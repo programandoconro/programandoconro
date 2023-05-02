971e62f370b4fffc5dd9e1e1330d977ac67e09b7
ts-rodrigo.diaz <ts-rodrigo.diaz@rakuten.com>
2023-05-02 18:03:46 +0900

    use default language and localHistory save language

--- src/scripts/setContent.js
+++ src/scripts/setContent.js
@@ -1,5 +1,7 @@
 import * as introductionText from "../../i18n/introdution.json";
 import * as profilesText from "../../i18n/profiles.json";
+import * as LANGUAGES from "../../i18n/languages.json";
+
 const intro = document.getElementById("intro");
 const profileTitle = document.getElementById("profile-title");
 const githubProfile = document.getElementById("github-profile");
@@ -25,16 +27,42 @@ const setContent = (language) => {
   linkedinProfile.innerText = joinText(profilesText.linkedin[language]);
   flickrProfile.innerText = joinText(profilesText.flickr[language]);
 };
-setContent("spanish");
+const getLanguage = () => {
+  const language = localStorage.getItem("language");
+  if (["es", "ja", "en"].includes(language)) {
+    return language;
+  }
+  return navigator.language.startsWith("es")
+    ? "es"
+    : navigator.language.startsWith("ja")
+    ? "ja"
+    : "en";
+};
+const language = getLanguage();
+const input = document.getElementsByTagName("input");
+
+if (language === "es") {
+  input[0].checked = true;
+  setContent(LANGUAGES.spanish);
+} else if (language === "ja") {
+  input[2].checked = true;
+  setContent(LANGUAGES.japanese);
+} else {
+  input[1].checked = true;
+  setContent(LANGUAGES.english);
+}
 const switches = document.getElementsByTagName("input");
 const [spanishButton, englishButton, japaneseButton] = switches;
 
 spanishButton.addEventListener("click", () => {
   setContent("spanish");
+  localStorage.setItem("language", "es");
 });
 englishButton.addEventListener("click", () => {
   setContent("english");
+  localStorage.setItem("language", "en");
 });
 japaneseButton.addEventListener("click", () => {
   setContent("japanese");
+  localStorage.setItem("language", "ja");
 });
